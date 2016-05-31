604a5004a01f97dcf19e775d9128d415d8519a5b
niyoufa <niyoufa@tmlsystem.com>
2016-05-31 23:58:31 +0800

    整理技术文档

--- python_init/__init__.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-class ManagementUtility(object):
-	def  __init__(self):
-		self.value = 0
-	def __getitem__(self):
-		return self.value
-	def __setitem__(self,value):
-		self.value = value
